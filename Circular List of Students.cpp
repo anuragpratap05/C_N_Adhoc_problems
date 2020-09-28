a4745d03b51aae93ec3beff4ce7bbb9c5f171b67
anuragpratap05 <56753626+anuragpratap05@users.noreply.github.com>
2020-09-28 21:06:07 +0530

    Create Circular List of Students.cpp

---
+++ Circular List of Students.cpp
@@ -0,0 +1,18 @@
+# C_N_Adhoc_problems
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,p;
+        cin>>n>>p;
+    
+        int sum=n+p;
+        cout<<(sum%12)<<endl;
+
+    }
+	return 0;
+}
